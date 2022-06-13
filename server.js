2315a3de5858feab9f8ca1e1b3e12ca6a2106ee9
Sandro Forster <sandroid@posteo.net>
2022-06-13 11:45:51 +0200

    Allow setting port with environment variable

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname, './index.html'));
 })
 
-const port =8080;
+const port = process.env.PORT || 8080;
 app.listen(port,()=>{
 console.log(`App running on ${port}`);
 });
